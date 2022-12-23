43543f410b6e0d7fc0aaccc050846d2545baa380
Yskandar Gas <33219452+Yskandar@users.noreply.github.com>
2022-12-23 06:45:04 -0800

    Time: 41 ms (27.43%), Space: 18.8 MB (9.98%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -8,15 +8,12 @@ public:
         vector<vector<int>> dp(N1 + 1, vector<int> (N2 + 1, 0));
 
         // Filling the DP array
-        for (int i = 0; i <= N1; i++) {
-            for (int j = 0; j <= N2; j++) {
-                if (i == 0 or j == 0) { continue; }
+        for (int i = 1; i <= N1; i++) {
+            for (int j = 1; j <= N2; j++) {
 
-                else {
-
-                    if (text1[i-1] == text2[j-1]) { dp[i][j] = 1 + dp[i - 1][j - 1]; }
-                    else { dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); }
-                }
+                if (text1[i-1] == text2[j-1]) { dp[i][j] = 1 + dp[i - 1][j - 1]; }
+                else { dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); }
+                
             }
 
         }
