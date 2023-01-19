09a30a353ade7c140a83efc9a01ca069ebd0adc3
Yskandar Gas <33219452+Yskandar@users.noreply.github.com>
2023-01-19 01:31:37 +0100

    Time: 1268 ms (11.63%), Space: 23 MB (5.92%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -38,35 +38,68 @@ class Solution:
 
             if dico_pacific.get(cell) == True:
                 return True
-            if dico_pacific.get(cell) == False:
-                return False
             if dico_visited_pacific.get(cell) == True:
                 return False
 
             dico_visited_pacific[cell] = True
+            if dico_flow.get(cell) is None:
+                i, j = cell
+                h = heights[i][j]
+                
+                # Filling the flow
+                if i+1 < n and heights[i+1][j] <= h:
+                    dico_flow[(i,j)] += [(i+1, j)]
+                if i-1 >= 0 and heights[i-1][j] <= h:
+                    dico_flow[(i,j)] += [(i-1, j)]
+                if j+1 < m and heights[i][j+1] <= h:
+                    dico_flow[(i,j)] += [(i, j+1)]
+                if j-1 >= 0 and heights[i][j-1] <= h:
+                    dico_flow[(i,j)] += [(i, j-1)]
+
+                # Near the ocean ?
+                if i==n-1 or j==m-1:
+                    dico_atlantic[(i,j)] = True
+                if i==0 or j==0:
+                    dico_pacific[(i,j)] = True
+            
             for next_cell in dico_flow[cell]:
                 if dfs_pacific(next_cell):
                     dico_pacific[cell] = True
                     return True
-            #dico_pacific[cell] = False
             dico_visited_pacific[cell] = False
             return False
 
         def dfs_atlantic(cell):
             if dico_atlantic.get(cell) == True:
                 return True
-            if dico_atlantic.get(cell) == False:
-                return False
             if dico_visited_atlantic.get(cell) == True:
                 return False
 
             dico_visited_atlantic[cell] = True
+            if dico_flow.get(cell) is None:
+                i, j = cell
+                h = heights[i][j]
+                
+                # Filling the flow
+                if i+1 < n and heights[i+1][j] <= h:
+                    dico_flow[(i,j)] += [(i+1, j)]
+                if i-1 >= 0 and heights[i-1][j] <= h:
+                    dico_flow[(i,j)] += [(i-1, j)]
+                if j+1 < m and heights[i][j+1] <= h:
+                    dico_flow[(i,j)] += [(i, j+1)]
+                if j-1 >= 0 and heights[i][j-1] <= h:
+                    dico_flow[(i,j)] += [(i, j-1)]
+
+                # Near the ocean ?
+                if i==n-1 or j==m-1:
+                    dico_atlantic[(i,j)] = True
+                if i==0 or j==0:
+                    dico_pacific[(i,j)] = True
             for next_cell in dico_flow[cell]:
                 if dfs_atlantic(next_cell):
                     dico_atlantic[cell] = True
                     return True
             dico_visited_atlantic[cell] = False
-            #dico_atlantic[cell] = False
             return False
 
         ans = []
