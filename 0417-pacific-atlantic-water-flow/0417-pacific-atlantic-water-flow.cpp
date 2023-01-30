1bffd59cc6b7237f16d9087d2631cec8d90849fb
Yskandar Gas <33219452+Yskandar@users.noreply.github.com>
2023-01-29 18:34:41 -0800

    Time: 137 ms (19.62%), Space: 46.6 MB (5.05%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,37 +1,55 @@
-class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+
+class Solution {
+
+public:
+
+
+    void dfs(int r, int c, int curr_height, vector<vector<int>>& heights, vector<vector<bool>>& ocean_set) {
+        int n = heights.size(), m = heights[0].size();
+        vector<int> pair = { r,c };
+        if (r<0 or c<0 or r==n or c==m or ocean_set[r][c] or heights[r][c] < curr_height) {
+            return;
+        }
+
+        ocean_set[r][c] = true;
+        dfs(r + 1, c, heights[r][c], heights, ocean_set);
+        dfs(r - 1, c, heights[r][c], heights, ocean_set);
+        dfs(r, c + 1, heights[r][c], heights, ocean_set);
+        dfs(r, c - 1, heights[r][c], heights, ocean_set);
+
+    }
+
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+
+        int n = heights.size(), m = heights[0].size();
+        vector<vector<bool>> pacific(n, vector<bool>(m, false));
+        vector<vector<bool>> atlantic(n, vector<bool>(m, false));
+
+        for (int c = 0; c < m; c++) {
+            dfs(0, c, heights[0][c], heights, pacific);
+            dfs(n-1, c, heights[n-1][c], heights, atlantic);
+        }
+        for (int r = 0; r < n; r++) {
+            dfs(r, 0, heights[r][0], heights, pacific);
+            dfs(r, m - 1, heights[r][m - 1], heights, atlantic);
+        }
         
-        
-        pacific, atlantic = set(), set()
-        n, m = len(heights), len(heights[0])
-        ans = []
-        
-        def dfs(r, c, curr_height, ocean_set):
-            
-            if r<0 or c<0 or r == n or c == m or heights[r][c] < curr_height or (r,c) in ocean_set:
-                return
-            
-            ocean_set.add((r, c))
-            dfs(r+1, c, heights[r][c], ocean_set)
-            dfs(r, c+1, heights[r][c], ocean_set)
-            dfs(r-1, c, heights[r][c], ocean_set)
-            dfs(r, c-1, heights[r][c], ocean_set)
-            
-        for r in range(n):
-            dfs(r, 0, heights[r][0], pacific)
-            dfs(r, m-1, heights[r][m-1], atlantic)
-        
-        for c in range(m):
-            dfs(n-1, c, heights[n-1][c], atlantic)
-            dfs(0, c, heights[0][c], pacific)
-        
-        for r in range(n):
-            for c in range(m):
-                if (r,c) in pacific and (r,c) in atlantic:
-                    ans.append([r,c])
-        
-        return ans
-        
-        
-            
-        
+        vector<vector<int>> ans;
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                vector<int> pair = { r,c };
+                if (pacific[r][c] and atlantic[r][c]) {
+                    ans.push_back(pair);
+                }
+
+
+            }
+
+        }
+        return ans;
+
+    }
+
+
+};
