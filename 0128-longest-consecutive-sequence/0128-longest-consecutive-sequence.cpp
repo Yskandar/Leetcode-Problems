4e5662b4a2be006f6f8684784b47d1da0cb481d1
Yskandar Gas <33219452+Yskandar@users.noreply.github.com>
2023-02-05 21:00:00 -0800

    Time: 624 ms (31.67%), Space: 50.8 MB (39.48%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,95 +1,34 @@
-class Node {
-public:
-	
-	int value;
-	std::vector<Node*> neighbors;
-
-	Node() {
-		value = NULL;
-		neighbors = {};
-	}
-
-	Node(int val, std::vector<Node*> n) {
-		value = val;
-		neighbors = n;
-	}
-	
-	Node(int val) {
-		value = val;
-		neighbors = {};
-	}
-
-	void add_neighbors(std::unordered_map<int, Node>& nodes) {
-
-		if (nodes.find(value + 1) != nodes.end()){
-			neighbors.push_back(&nodes[value + 1]);
-		}
-		
-	}
-
-
-};
-
-int dfs(Node& node, std::unordered_map<int, int>& counts) {
-
-	if (counts.find(node.value) != counts.end()) {
-		return counts[node.value];
-	}
-
-	if (node.neighbors.size() == 0) {
-		counts[node.value] = 1;
-		return counts[node.value];
-	}
-
-	else {
-		counts[node.value] = 1 + dfs(*node.neighbors[0], counts);
-		return counts[node.value];
-	}
-
-}
-
-
 
 class Solution {
-
 public:
 
-	int longestConsecutive(std::vector<int>& nums) {
+	int longestConsecutive(std::vector<int> nums) {
 
-		std::unordered_map<int, Node> nodes;
-
-		// create the graph
+		std::unordered_set<int> numset;
+		int ans = 0;
 		for (int i = 0; i < nums.size(); i++) {
-			nodes[nums[i]] = Node(nums[i]);
+			numset.insert(nums[i]);
 		}
 
-		// instanciate neighbors
 		for (int i = 0; i < nums.size(); i++) {
-			nodes[nums[i]].add_neighbors(nodes);
-		}
+			if (numset.find(nums[i] - 1) == numset.end()) {
+				int length = 1;
+				while (numset.find(nums[i] + length) != numset.end()) {
+					length += 1;
 
-		// apply dfs for each node
-		int count = 0;
-		std::unordered_map<int, bool> visited;
-		std::unordered_map<int, int> counts;
-
-
-		for (int i = 0; i < nums.size(); i++) {
-			if (visited[nums[i]]) {
-				continue;
+				}
+				ans = std::max(ans, length);
 			}
-			
-			visited[nums[i]] = true;
-			count = std::max(count, dfs(nodes[nums[i]],counts));
 
 		}
 
-		return count;
-
-
+		return ans;
 
 
 
 	}
+
+
+
 
 };
