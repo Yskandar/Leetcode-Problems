0be08977a776adf944d20ed61076ab7180d28d24
Yskandar Gas <33219452+Yskandar@users.noreply.github.com>
2023-03-29 12:56:46 -0700

    Time: 163 ms (38.70%), Space: 13.8 MB (65.03%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,27 +1,20 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         
-        
-        
         count = 0
         n = len(s)
-        # Looking for palindromic centers
         
-        
+        # Looking for palindromic centers of odd length palindromes
         for i in range(0, len(s)):
             j = 0
+            k = 0
             while i-j >= 0 and i+j < n and s[i-j] == s[i+j]:
                 count += 1
                 j += 1
-                
-        for i in range(0, len(s)-1):
-            j = 0
-            if s[i] != s[i+1]:
-                continue
-            else:
-                while i-j >= 0 and i+j+1 < n and s[i-j] == s[i+j+1]:
-                    count+=1
-                    j+=1
+            
+            while i-k >= 0 and i+k+1 < n and s[i] == s[i+1] and s[i-k] == s[i+k+1]:
+                count += 1
+                k+=1
         
         return count
                 
